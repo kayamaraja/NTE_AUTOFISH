c8af4e2b69a431155885a2afdbb25c46682cd8ac
Kayan <74180288+kayamaraja@users.noreply.github.com>
2026-05-06 02:13:12 +0800

    Update libraries.bat

--- libraries.bat
+++ libraries.bat
@@ -1,13 +1,1 @@
-@echo off
-chcp 65001 > nul
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    powershell -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
-cd /d "%~dp0"
 
-python -m pip install --upgrade pip
-pip install -r req.txt
-python fishing_bot.py[cite: 1, 3]
-pause
